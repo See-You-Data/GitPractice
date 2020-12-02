3ec04887f63af11f439bec2c4fa2b25a992afca0
JBSellman <32461430+JBSellman@users.noreply.github.com>
2020-12-02 11:37:36 +0000

    commented code to try to force conflict
    copied the function and made minor changes

--- Printing Function.py
+++ Printing Function.py
@@ -4,12 +4,25 @@
 # Would be good to include function documentation (would be good to agree on a documentation standard i.e. NumPy's style).
 # Also it'd be best if our changes edit the code of others as this will cause merge conflicts which are presumably a bit trickier to fix & thus good practice. 
 
-def printer(input,case=None):
+#JS Commented out lines of code to force conflict, this can happen with improper git workflow
+
+# def printer(input,case=None):
+#     if case == 'title':
+#         print(input.title())
+#     elif case == 'upper':
+#         print(input.upper())
+#     else:
+#         print(input)
+#
+# printer('hello',case='upper')
+
+
+def print_func(input,case=None):
     if case == 'title':
-        print(input.title())
+        print(2 * input.title())
     elif case == 'upper':
-        print(input.upper())
+        print(2 *input.upper())
     else:
-        print(input)
-  
-printer('hello',case='upper')
+        print(2 * input)
+
+print_func('hello',case='upper')
